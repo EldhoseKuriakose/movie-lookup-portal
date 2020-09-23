3a49fd5f8e200950919babf552656fa7ffa9b3fe
Eldhose Kuriakose <eldhosekuriakose96@gmail.com>
2020-09-23 14:26:13 +0530

    movie-card

---
+++ src/components/MovieCard/MovieCard.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './MovieCard.styles.scss';
+
+const MovieCard = () => (
+    <div className="movie-card-container">
+
+    </div>
+);
+
+export default MovieCard;
--- src/components/MovieList/MovieList.component.jsx
+++ src/components/MovieList/MovieList.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import MovieCard from '../MovieCard/MovieCard.component';
 import './MovieList.styles.scss';
 
 const MovieList = () => (
     <div className="movie-list-container">
-
+        <MovieCard />
     </div>
 );
 
